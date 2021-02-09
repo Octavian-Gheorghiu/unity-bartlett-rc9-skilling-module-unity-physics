6847874d4b5ad8c52df6eb5324b458dd74583599
Octavian Gheorghiu <octavian.gheorghiu@yahoo.com>
2021-02-09 08:20:38 +0000

    Session 6
    Added collision triggers and finalised the game logic.

--- Assets/Common/Scripts/Systems/SpwanSystem.cs
+++ Assets/Common/Scripts/Systems/SpwanSystem.cs
@@ -13,6 +13,7 @@ public class SpwanSystem : SystemBase
         Entities.ForEach((ref Spawner spawner, in Translation trns, in Rotation rot) => {
             if (!EntityManager.Exists(spawner.spawnObject))
             {
+                // Final assignmnt maybe spawn more than one spawnPrefab using a for loop
                 spawner.spawnObject = EntityManager.Instantiate(spawner.spawnPrefab);
                 EntityManager.SetComponentData(spawner.spawnObject, trns);
                 EntityManager.SetComponentData(spawner.spawnObject, rot);
